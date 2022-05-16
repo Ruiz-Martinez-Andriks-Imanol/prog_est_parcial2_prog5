b3b74532e974aba62fcef31f486f91a923170da9
Andriks Imanol Ruiz Martínez <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-16 17:56:37 -0500

    Update prog2_5.cpp

--- prog2_5.cpp
+++ prog2_5.cpp
@@ -1,34 +1,36 @@
-/* Autor: Andriks Imanol Ruiz M·rtinez, Realizado: 31/03/2022
-Escuela: Universidad del Valle de MÈxico Campus Villahermosa
-Materia: ProgramaciÛn Estructurada
+/* Autor: Andriks Imanol Ruiz M√°rtinez, Realizado: 31/03/2022
+Escuela: Universidad del Valle de M√©xico Campus Villahermosa
+Materia: Programaci√≥n Estructurada
 Ciclo: 01/2022
 
-Este es un programa en Lenguaje C de la materia de ProgramaciÛn Estructurada
+Este es un programa en Lenguaje C de la materia de Programaci√≥n Estructurada
 Muestra el uso de:
 
     -Variables en cadena y unsigned
     -printf para mostrar mensajes y variables
     -scanf
-    -El uso de include para las librerÌas
+    -El uso de include para las librer√≠as
     -uso de condicionales
-    -Operadores lÛgicos
-    -Comentarios para la documentaciÛn interna del programa
+    -Operadores l√≥gicos
+    -Comentarios para la documentaci√≥n interna del programa
 */
 #include<stdio.h>
 int main(){
+	//Declaraci√≥n
 	unsigned char c;
-	//Ejemplo de impresiÛn de caracteres especiales
+	//Proceso
+	//Ejemplo de impresi√≥n de caracteres especiales
 	printf("Pa%cuelo, Canci%cn\n",164,162);
 	printf("Introduce un caracter: ");
 	scanf("%c", &c);
 	//printf("Se introdujo el caracter <%c> y su codigo ASCII es %d \n ", dato, dato);
 	//Probar si es una letra
-	//	Mayusculas			minusculas			acentos y Ò			¸			È	
-	if(c>='A' && c<='Z' || c>='a' && c<='z' ||  c>=160 && c<=165 || c==129 || c==130){
+	//	Mayusculas			minusculas			acentos y √±			√º			√©	
+	if(c>='A' && c<='Z' || c>='a' && c<='z' ||  c>=160 && c<=165 || c==129 || c==130){ //Condicionalrs
 		printf("<%c> Es una letra\n", c);
 	}
 	else if(c>='0' && c<='9'){
-		printf("<%c> Es un numero\n", c);
+		printf("<%c> Es un numero\n", c); //Salida
 	}
 	else {
 		printf("<%c> Es un caracter especial\n", c);
